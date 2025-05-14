3acf4832400a5677113923f9936bb37d42082466
Jakub Bieniek <bienio59@gmail.com>
2025-05-14 15:40:32 +0200

    Docker config is now settled by default

--- app/__init__.py
+++ app/__init__.py
@@ -10,10 +10,11 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     #docker config
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@db:5432/poke_db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@db:5432/poke_db'
 
     #localhost config
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/poke_db'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/poke_db'
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = 'super-secret'
     db.init_app(app)
